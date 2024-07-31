f54e8afd0e32d700c8443014f8160f232a9601c8
Sylwia B <sylwia.barteczko95@gmail.com>
2024-07-31 08:25:15 -0700

    2 Zmiany przed zajeciami 31.07

--- src/main/java/com/example/spring_boot/UserController.java
+++ src/main/java/com/example/spring_boot/UserController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@RequestMapping("/api/users")
+@RequestMapping("/api/user")
 public class UserController {
 
     @Autowired
